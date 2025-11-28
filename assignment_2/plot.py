09554078f1c2ada91442f7f9ceef1f0ecbb793de
misteraxl <axel.lagerstedt@icloud.com>
2025-11-28 12:09:21 +0100

    finished task 3, lab 2

--- assignment_2/plot.py
+++ assignment_2/plot.py
@@ -1,22 +1,13 @@
-import numpy as py
+import numpy as np, scipy.stats as st
 import matplotlib.pyplot as plt
 
-# plt.plot([1, 2, 3, 4])
-# plt.ylabel('some numbers')
-# # plt.show()
-
-# Specify the path to your file
 file_path = 'src/assignment_2/5k_run_1'
 
-# Use 'r' for read mode
 with open(file_path, 'r') as file:
-    # Option 1: Read the entire file content into a single string
-    # content = file.read()
-    # print("--- Entire File Content ---")
-    # print(content)
-    
-    # # Option 2: Read file line-by-line into a list
-    file.seek(0) # Reset file pointer to the beginning for re-reading
+    file.seek(0)
     lines = file.readlines()
-    print("--- First 5 Lines ---")
-    print(len(lines))
+
+values = np.array([int(k) for k in lines])
+
+ci = st.t.interval(0.95, len(values)-1, loc=np.mean(values), scale=st.sem(values))
+print(ci[1] - ci[0])
